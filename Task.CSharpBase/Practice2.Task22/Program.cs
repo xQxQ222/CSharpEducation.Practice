9e4ae68aa4b766ca269365286cda73d53ee439eb
Ivan <nerounxqxq@gmail.com>
2025-05-31 16:53:57 +0400

    Доделана практика №2

---
+++ Task.CSharpBase/Practice2.Task22/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Practice2.Task22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Игра: УГАДАЙ ЧИСЛО!");
+            Random r = new Random();
+            int randomNumber = r.Next(1, 100);
+            int maxTryCount = r.Next(2, 20);
+            Console.WriteLine($"У вас будет {maxTryCount} попыток угадать число!");
+            int userInput = 0;
+            int i = 0;
+            do
+            {
+                i++;
+                Console.Write($"Попытка №{i}\nВведите число: ");
+                try
+                {
+                    userInput = int.Parse(Console.ReadLine());
+                    if (userInput > randomNumber)
+                    {
+                        Console.WriteLine("Ваше число больше загаданного!");
+                    }
+                    else if (userInput < randomNumber)
+                    {
+                        Console.WriteLine("Ваше число меньше загаданного!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            while (userInput != randomNumber && i < maxTryCount);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Вы победили! Поздравляем!\nНа отгадывание числа ушло {i} попыток");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ Task.CSharpBase/Practice2.Task24/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Practice2.Task24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var e in Enum.GetNames(typeof(DayOfWeek)))
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+
+    enum DayOfWeek
+    {
+        Понедельник,
+        Вторник,
+        Среда,
+        Четверг,
+        Пятница,
+        Суббота,
+        Воскресенье
+    }
+}
